045e6f298cb14e970bba85c7ec6e70eb652dc974
Lee Morgan <lmorgan@footlocker.com>
2016-06-29 17:33:09 -0400

    Updates to leave bower_components dir on clean, and plug in ui-bootstrap.

---
+++ public/scripts/app.module.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var app = angular.module('plunker', [
+
+  'ui.bootstrap'
+
+]);
+
+app.controller('MainCtrl', function($scope) {
+  $scope.name = 'World';
+})
+//
+// app.controller('NavBarCtrl', ['$scope', function($scope) {
+//   $scope.isCollapsed = true;
+// }]);
